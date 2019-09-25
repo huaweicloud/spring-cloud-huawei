16677a12f95a3f2dd246725fb8d3c1638e2f3710
wangqijun <wangqj2011@sohu.com>
2019-09-25 10:30:19 +0800

    add example for dtm

--- spring-cloud-huawei-dtm/src/main/java/org/springframework/cloud/dtm/rest/RestTemplateForDtmAutoConfiguration.java
+++ spring-cloud-huawei-dtm/src/main/java/org/springframework/cloud/dtm/rest/RestTemplateForDtmAutoConfiguration.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -16,6 +18,8 @@ import org.springframework.web.client.RestTemplate;
  * @Date 12:45 2019-09-18
  **/
 public class RestTemplateForDtmAutoConfiguration {
+  private static final Logger LOGGER = LoggerFactory.getLogger(RestTemplateForDtmAutoConfiguration.class);
+
   @Autowired(required = false)
   private Collection<RestTemplate> restTemplates;
 
@@ -29,6 +33,7 @@ public class RestTemplateForDtmAutoConfiguration {
 
   @PostConstruct
   public void init() {
+    LOGGER.info("RestTemplateForDtmAutoConfiguration init..");
     if (this.restTemplates != null) {
       for (RestTemplate restTemplate : restTemplates) {
         List<ClientHttpRequestInterceptor> interceptors = new ArrayList<ClientHttpRequestInterceptor>(
---
+++ spring-cloud-huawei-sample/dtm-demo/ticket/src/main/java/org/springframework/cloud/sample/dtm/TicketAppMain.java
@@ -0,0 +1,17 @@
+package org.springframework.cloud.sample.dtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author wangqijun
+ * @Date 09:53 2019-09-23
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TicketAppMain {
+  public static void main(String[] args) {
+    SpringApplication.run(TicketAppMain.class, args);
+  }
+}
