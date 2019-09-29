f3e1c66aae50836efbd92203e83bbbd871bfc203
Jon Wang <douliw@126.com>
2019-09-29 09:30:03 +0800

    add license (#23)

---
+++ spring-cloud-huawei-dtm/src/test/java/org/springframework/cloud/dtm/consumer/rest/DtmRestTemplateInterceptorTest.java
@@ -0,0 +1,14 @@
+package org.springframework.cloud.dtm.consumer.rest;
+
+import org.junit.Test;
+
+/**
+ * @Author wangqijun
+ * @Date 18:04 2019-09-27
+ **/
+public class DtmRestTemplateInterceptorTest {
+
+  @Test
+  public void intercept() {
+  }
+}
