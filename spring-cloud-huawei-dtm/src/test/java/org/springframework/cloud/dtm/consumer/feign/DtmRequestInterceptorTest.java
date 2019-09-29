3bac5de4b293df814ecd0f6d382e681ab0c5e998
wangqijun <wangqj2011@sohu.com>
2019-09-29 11:31:11 +0800

    add ut for dtm

---
+++ spring-cloud-huawei-dtm/src/test/java/org/springframework/cloud/dtm/consumer/feign/DtmRequestInterceptorTest.java
@@ -0,0 +1,27 @@
+package org.springframework.cloud.dtm.consumer.feign;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.springframework.cloud.dtm.util.DtmConstants;
+
+import com.huawei.middleware.dtm.client.context.DTMContext;
+
+import feign.RequestTemplate;
+
+/**
+ * @Author wangqijun
+ * @Date 10:52 2019-09-29
+ **/
+public class DtmRequestInterceptorTest {
+
+  @Test
+  public void apply() {
+    DtmRequestInterceptor dtmRequestInterceptor = new DtmRequestInterceptor();
+    RequestTemplate template = new RequestTemplate();
+    DTMContext dtmContext = DTMContext.getDTMContext();
+    dtmContext.setGlobalTxId(100);
+    dtmRequestInterceptor.apply(template);
+    assertNotNull(template.header(DtmConstants.DTM_CONTEXT));
+  }
+}
