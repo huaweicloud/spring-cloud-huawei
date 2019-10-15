da66a0ff40a5463708195bc0ed65f96db15b4134
wangqijun <wangqj2011@sohu.com>
2019-10-15 10:38:03 +0800

    add config and modify dtm-demo structure

---
+++ spring-cloud-huawei-sample/dtm-demo/reserve-feign/scripts/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd `dirname $0`
+BIN_DIR=`pwd`
+cd ..
+DEPLOY_DIR=`pwd`
+CONF_DIR=$DEPLOY_DIR/conf
+
+
+LOGS_DIR=""
+if [ -n "$LOGS_FILE" ]; then
+    LOGS_DIR=`dirname $LOGS_FILE`
+else
+    LOGS_DIR=$DEPLOY_DIR/log
+fi
+if [ ! -d $LOGS_DIR ]; then
+    mkdir $LOGS_DIR
+fi
+STDOUT_FILE=$LOGS_DIR/stdout.log
+
+LIB_DIR=$DEPLOY_DIR/lib
+LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
+
+echo -e "Starting success ...\c"
+java -classpath $CONF_DIR:$LIB_JARS org.springframework.cloud.sample.dtm.ReserveFeignAppMain
