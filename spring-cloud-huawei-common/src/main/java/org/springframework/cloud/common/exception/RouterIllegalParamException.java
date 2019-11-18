d67985e794187bc3ba890b42943f02dba92f18ef
GuoYL <53255576+GuoYL123@users.noreply.github.com>
2019-11-18 11:53:36 +0800

    canary release rename to router (#55)

---
+++ spring-cloud-huawei-common/src/main/java/org/springframework/cloud/common/exception/RouterIllegalParamException.java
@@ -0,0 +1,16 @@
+package org.springframework.cloud.common.exception;
+
+/**
+ * @Author GuoYl123
+ * @Date 2019/11/4
+ **/
+public class RouterIllegalParamException extends ServiceCombRuntimeException {
+
+  public RouterIllegalParamException(String message) {
+    super(message);
+  }
+
+  public RouterIllegalParamException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
