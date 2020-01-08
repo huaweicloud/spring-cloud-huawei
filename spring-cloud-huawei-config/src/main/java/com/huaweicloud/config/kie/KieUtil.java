6c99c4f1d507565fe2d4e0d0283f9b9272aabb03
GuoYL <gylgeek@gmail.com>
2020-01-08 16:02:18 +0800

    add all file update

--- spring-cloud-huawei-config/src/main/java/com/huaweicloud/config/kie/KieUtil.java
+++ spring-cloud-huawei-config/src/main/java/com/huaweicloud/config/kie/KieUtil.java
@@ -1,16 +1,27 @@
 package com.huaweicloud.config.kie;
 
+import com.huaweicloud.common.exception.ServiceCombRuntimeException;
 import com.huaweicloud.config.ServiceCombConfigProperties;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.io.ByteArrayResource;
 
 /**
  * @Author GuoYl123
  * @Date 2020/1/7
  **/
 public class KieUtil {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(KieUtil.class);
 
   public static Map<String, String> getConfigByLabel(
       ServiceCombConfigProperties serviceCombConfigProperties, KVResponse resp) {
@@ -40,16 +51,68 @@ public class KieUtil {
         }
       }
     }
-    //todo :priority
+    //todo : kv is priority
     for (KVDoc kvDoc : appList) {
+      resultMap.putAll(processValueType(kvDoc));
       resultMap.put(kvDoc.getKey(), kvDoc.getValue());
     }
     for (KVDoc kvDoc : serviceList) {
+      resultMap.putAll(processValueType(kvDoc));
       resultMap.put(kvDoc.getKey(), kvDoc.getValue());
     }
     for (KVDoc kvDoc : versionList) {
+      resultMap.putAll(processValueType(kvDoc));
       resultMap.put(kvDoc.getKey(), kvDoc.getValue());
     }
     return resultMap;
+  }
+
+
+  // todo : first time update to kie server
+  public static Map<String, String> processValueType(KVDoc kvDoc) {
+    if (!isValidName(kvDoc.getKey())) {
+      return Collections.emptyMap();
+    }
+    ValueType vtype;
+    try {
+      vtype = ValueType.valueOf(kvDoc.getValueType());
+    } catch (IllegalArgumentException e) {
+      throw new ServiceCombRuntimeException("value type not support");
+    }
+    Properties properties = new Properties();
+    try {
+      if (vtype == (ValueType.yaml) || vtype == (ValueType.yml)) {
+        YamlPropertiesFactoryBean yamlFactory = new YamlPropertiesFactoryBean();
+        yamlFactory.setResources(new ByteArrayResource(kvDoc.getValue().getBytes()));
+        properties = yamlFactory.getObject();
+      } else if (vtype == (ValueType.properties)) {
+        properties.load(new StringReader(kvDoc.getValue()));
+      }
+      return toMap(properties);
+    } catch (Exception e) {
+      LOGGER.error("read config failed");
+    }
+    return Collections.emptyMap();
+  }
+
+  private static boolean isValidName(String name) {
+    return name.equals("application.yaml") || name.equals("application.yml") ||
+        name.equals("bootstrap.yaml") || name.equals("bootstrap.yml") ||
+        name.equals("application.properties") || name.equals("bootstrap.properties");
+  }
+
+  private static Map<String, String> toMap(Properties properties) {
+    Map<String, String> result = new HashMap<>();
+    Enumeration<String> keys = (Enumeration<String>) properties.propertyNames();
+    while (keys.hasMoreElements()) {
+      String key = keys.nextElement();
+      Object value = properties.getProperty(key);
+      if (value != null) {
+        result.put(key, ((String) value).trim());
+      } else {
+        result.put(key, null);
+      }
+    }
+    return result;
   }
 }
---
+++ spring-cloud-huawei-config/src/main/java/com/huaweicloud/config/kie/ValueType.java
@@ -0,0 +1,14 @@
+package com.huaweicloud.config.kie;
+
+/**
+ * @Author GuoYl123
+ * @Date 2020/1/8
+ **/
+public enum ValueType {
+  yml,
+  yaml,
+  string,
+  text,
+  json,
+  properties
+}
