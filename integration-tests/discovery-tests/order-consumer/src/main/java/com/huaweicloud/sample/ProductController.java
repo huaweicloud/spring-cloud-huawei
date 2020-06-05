680d18d9563d797926880b6e4ae81bd8afbdd4b5
liubao <bismy@qq.com>
2020-06-05 17:45:16 +0800

    [#198]first add some test cases and refactor code with two swagger generation mode

--- integration-tests/discovery-tests/order-consumer/src/main/java/com/huaweicloud/sample/ProductController.java
+++ integration-tests/discovery-tests/order-consumer/src/main/java/com/huaweicloud/sample/ProductController.java
@@ -4,14 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ProductController {
-  String url = "http://127.0.0.1:8088";
-
-  RestTemplate template = new RestTemplate();
-
   @Autowired
   private FeignService feignService;
 
