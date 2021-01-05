379faf23c8248b08bad40969d703281b17feb351
GuoYL <53255576+GuoYL123@users.noreply.github.com>
2021-01-05 10:08:11 +0800

    fix demo bug (#318)

--- spring-cloud-huawei-sample/swagger-demo/javachassis-consumer/src/main/java/com/huaweicloud/sample/AppMain.java
+++ spring-cloud-huawei-sample/swagger-demo/javachassis-consumer/src/main/java/com/huaweicloud/sample/AppMain.java
@@ -25,7 +25,6 @@ import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
  **/
 public class AppMain {
     public static void main(String[] args) throws Exception {
-        Log4jUtils.init();
         BeanUtils.init();
     }
 }
