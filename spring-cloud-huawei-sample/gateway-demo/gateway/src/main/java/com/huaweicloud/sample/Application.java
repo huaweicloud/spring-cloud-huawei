86e46e41657c90327908f05da4247fe8ad8941f0
GuoYL <gylgeek@gmail.com>
2021-01-05 09:46:18 +0800

    fix demo bug

--- spring-cloud-huawei-sample/gateway-demo/gateway/src/main/java/com/huaweicloud/sample/Application.java
+++ spring-cloud-huawei-sample/gateway-demo/gateway/src/main/java/com/huaweicloud/sample/Application.java
@@ -3,9 +3,6 @@ package com.huaweicloud.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @Author GuoYl123
