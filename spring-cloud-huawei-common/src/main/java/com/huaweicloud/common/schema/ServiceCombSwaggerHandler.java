93b2ec0bcab441d07c50384bf9a1e21332fcc03d
liubao68 <bismy@qq.com>
2021-04-02 15:52:21 +0800

    [#371]refactoring register PART4: swagger handler

--- spring-cloud-huawei-common/src/main/java/com/huaweicloud/common/schema/ServiceCombSwaggerHandler.java
+++ spring-cloud-huawei-common/src/main/java/com/huaweicloud/common/schema/ServiceCombSwaggerHandler.java
@@ -20,15 +20,9 @@ package com.huaweicloud.common.schema;
 import java.util.List;
 import java.util.Map;
 
-/**
- * @Author GuoYl123
- * @Date 2019/12/17
- **/
 public interface ServiceCombSwaggerHandler {
 
   void init(String appName, String serviceName);
-
-  void registerSwagger(String microserviceId, List<String> schemas);
 
   List<String> getSchemaIds();
 
