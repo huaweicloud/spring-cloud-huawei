71ce489d88daea3f0285b0991f78c04314e54385
liubao68 <bismy@qq.com>
2021-05-20 12:01:38 +0800

    [#327]using the common config API to replace current implementation

--- spring-cloud-huawei-config/src/main/java/com/huaweicloud/config/ServiceCombConfigBootstrapConfiguration.java
+++ spring-cloud-huawei-config/src/main/java/com/huaweicloud/config/ServiceCombConfigBootstrapConfiguration.java
@@ -17,46 +17,205 @@
 
 package com.huaweicloud.config;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.servicecomb.config.center.client.AddressManager;
+import org.apache.servicecomb.config.center.client.ConfigCenterClient;
+import org.apache.servicecomb.config.center.client.ConfigCenterManager;
+import org.apache.servicecomb.config.center.client.model.QueryConfigurationsRequest;
+import org.apache.servicecomb.config.center.client.model.QueryConfigurationsResponse;
+import org.apache.servicecomb.config.kie.client.KieClient;
+import org.apache.servicecomb.config.kie.client.KieConfigManager;
+import org.apache.servicecomb.config.kie.client.model.ConfigConstants;
+import org.apache.servicecomb.config.kie.client.model.ConfigurationsRequest;
+import org.apache.servicecomb.config.kie.client.model.ConfigurationsResponse;
+import org.apache.servicecomb.config.kie.client.model.KieAddressManager;
+import org.apache.servicecomb.foundation.auth.AuthHeaderProvider;
+import org.apache.servicecomb.http.client.auth.RequestAuthHeaderProvider;
+import org.apache.servicecomb.http.client.common.HttpTransport;
+import org.apache.servicecomb.http.client.common.HttpTransportFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.refresh.ContextRefresher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.util.StringUtils;
 
-import com.huaweicloud.common.exception.ServiceCombRuntimeException;
+import com.huaweicloud.common.event.EventManager;
 import com.huaweicloud.common.transport.ServiceCombAkSkProperties;
-import com.huaweicloud.common.transport.ServiceCombRBACProperties;
 import com.huaweicloud.common.transport.ServiceCombSSLProperties;
-import com.huaweicloud.config.client.ServiceCombConfigClient;
-import com.huaweicloud.config.client.ServiceCombConfigClientBuilder;
+import com.huaweicloud.common.transport.TransportUtils;
+import com.huaweicloud.common.util.URLUtil;
 
 @Configuration
 @EnableConfigurationProperties(ServiceCombConfigProperties.class)
 @ConditionalOnProperty(name = "spring.cloud.servicecomb.config.enabled", matchIfMissing = true)
 public class ServiceCombConfigBootstrapConfiguration {
-  @Bean
-  public ServiceCombConfigClient serviceCombConfigClient(
-      ServiceCombConfigProperties serviceCombConfigProperties, ServiceCombRBACProperties serviceCombRBACProperties,
-      ServiceCombAkSkProperties serviceCombAkSkProperties, ServiceCombSSLProperties serviceCombSSLProperties) {
-    ServiceCombConfigClientBuilder builder = new ServiceCombConfigClientBuilder();
-    if (!StringUtils.isEmpty(serviceCombAkSkProperties.getEnable())) {
-      throw new ServiceCombRuntimeException(
-          "config credentials.enable has change to credentials.enabled ,old names are no longer supported, please change it.");
-    }
-    builder.setServiceCombSSLProperties(serviceCombSSLProperties)
-        .setServiceCombRBACProperties(serviceCombRBACProperties)
-        .setServiceCombConfigProperties(serviceCombConfigProperties)
-        .setServiceCombAkSkProperties(serviceCombAkSkProperties);
-    return builder.createServiceCombConfigClient();
-  }
+  private static final String DEFAULT_PROJECT = "default";
 
   @Bean
-  public ServiceCombPropertySourceLocator serviceCombPropertySourceLocator(
-      ServiceCombConfigProperties serviceCombConfigProperties,
-      ServiceCombConfigClient serviceCombConfigClient,
-      ServiceCombAkSkProperties serviceCombAkSkProperties) {
-    return new ServiceCombPropertySourceLocator(serviceCombConfigProperties,
-        serviceCombConfigClient,
-        serviceCombAkSkProperties.getProject());
+  public ConfigWatch configWatch(ContextRefresher contextRefresher) {
+    return new ConfigWatch(contextRefresher);
+  }
+
+  @Configuration
+  @ConditionalOnProperty(name = "spring.cloud.servicecomb.config.serverType", havingValue = "config-center", matchIfMissing = true)
+  public static class ConfigCenterConfiguration {
+    private final Map<String, Object> configurations = new ConcurrentHashMap<>();
+
+    private QueryConfigurationsRequest queryConfigurationsRequest;
+
+    @Bean
+    public AddressManager configCenterAddressManager(ServiceCombConfigProperties configProperties,
+        ServiceCombAkSkProperties serviceCombAkSkProperties) {
+      List<String> addresses = URLUtil.getEnvConfigUrl();
+      if (addresses.isEmpty()) {
+        addresses = URLUtil.dealMultiUrl(configProperties.getServerAddr());
+      }
+      return new AddressManager(serviceCombAkSkProperties.getProject(), addresses);
+    }
+
+    @Bean
+    public HttpTransport configCenterHttpTransport(AddressManager addressManager,
+        ServiceCombSSLProperties serviceCombSSLProperties,
+        List<AuthHeaderProvider> authHeaderProviders) {
+
+      return HttpTransportFactory
+          .createHttpTransport(
+              TransportUtils.createSSLProperties(addressManager.sslEnabled(), serviceCombSSLProperties),
+              getRequestAuthHeaderProvider(authHeaderProviders));
+    }
+
+    @Bean
+    public ConfigCenterClient configCenterClient(HttpTransport configCenterHttpTransport,
+        AddressManager configCenterAddressManager) {
+      return new ConfigCenterClient(configCenterAddressManager, configCenterHttpTransport);
+    }
+
+    @Bean
+    public ConfigCenterManager configCenterManager(ConfigCenterClient configCenterClient,
+        ServiceCombConfigProperties configProperties) {
+      queryConfigurationsRequest = createQueryConfigurationsRequest(configProperties);
+      QueryConfigurationsResponse response = configCenterClient
+          .queryConfigurations(queryConfigurationsRequest);
+      configurations.putAll(response.getConfigurations());
+      queryConfigurationsRequest.setRevision(response.getRevision());
+      ConfigCenterManager configCenterManager = new ConfigCenterManager(configCenterClient, EventManager.getEventBus(),
+          configurations);
+      configCenterManager.setQueryConfigurationsRequest(queryConfigurationsRequest);
+      configCenterManager.startConfigCenterManager();
+      return configCenterManager;
+    }
+
+    @Bean
+    @DependsOn("configCenterManager")
+    public ServiceCombPropertySourceLocator serviceCombPropertySourceLocator(
+        ServiceCombConfigProperties serviceCombConfigProperties,
+        ServiceCombAkSkProperties serviceCombAkSkProperties) {
+      return new ServiceCombPropertySourceLocator(configurations);
+    }
+
+    private QueryConfigurationsRequest createQueryConfigurationsRequest(ServiceCombConfigProperties configProperties) {
+      QueryConfigurationsRequest request = new QueryConfigurationsRequest();
+      request.setApplication(configProperties.getAppName());
+      request.setServiceName(configProperties.getServiceName());
+      request.setVersion(configProperties.getVersion());
+      request.setEnvironment(configProperties.getEnv());
+      // 需要设置为 null， 并且 query 参数为 revision=null 才会返回 revision 信息。 revision = 是不行的。
+      request.setRevision(null);
+      return request;
+    }
+  }
+
+  @Configuration
+  @ConditionalOnProperty(name = "spring.cloud.servicecomb.config.serverType", havingValue = "kie")
+  class KieConfiguration {
+    private final Map<String, Object> configurations = new ConcurrentHashMap<>();
+
+    private ConfigurationsRequest configurationsRequest;
+
+    @Bean
+    public KieAddressManager configKieAddressManager(ServiceCombConfigProperties configProperties,
+        ServiceCombAkSkProperties serviceCombAkSkProperties) {
+      List<String> addresses = URLUtil.getEnvConfigUrl();
+      if (addresses.isEmpty()) {
+        addresses = URLUtil.dealMultiUrl(configProperties.getServerAddr());
+      }
+      return createKieAddressManager(addresses, configProperties, serviceCombAkSkProperties);
+    }
+
+    public KieAddressManager createKieAddressManager(List<String> addresses,
+        ServiceCombConfigProperties configProperties,
+        ServiceCombAkSkProperties serviceCombAkSkProperties) {
+      Properties properties = new Properties();
+      Map<String, String> configKey = new HashMap<>();
+      properties.setProperty(ConfigConstants.KEY_PROJECT,
+          StringUtils.isEmpty(serviceCombAkSkProperties.getProject()) ? DEFAULT_PROJECT
+              : serviceCombAkSkProperties.getProject());
+      properties
+          .setProperty(ConfigConstants.KEY_ENABLELONGPOLLING,
+              Boolean.toString(configProperties.getEnableLongPolling()));
+      properties.setProperty(ConfigConstants.KEY_POLLINGWAITSEC,
+          Integer.toString(configProperties.getWatch().getPollingWaitTimeInSeconds()));
+
+      configKey.put(ConfigConstants.KEY_PROJECT, ConfigConstants.KEY_PROJECT);
+      configKey.put(ConfigConstants.KEY_ENABLELONGPOLLING, ConfigConstants.KEY_ENABLELONGPOLLING);
+      configKey.put(ConfigConstants.KEY_POLLINGWAITSEC, ConfigConstants.KEY_POLLINGWAITSEC);
+
+      return new KieAddressManager(properties, addresses, configKey);
+    }
+
+    @Bean
+    public HttpTransport configKieHttpTransport(KieAddressManager addressManager,
+        ServiceCombSSLProperties serviceCombSSLProperties,
+        List<AuthHeaderProvider> authHeaderProviders) {
+
+      return HttpTransportFactory
+          .createHttpTransport(
+              TransportUtils.createSSLProperties(addressManager.sslEnabled(), serviceCombSSLProperties),
+              getRequestAuthHeaderProvider(authHeaderProviders));
+    }
+
+    @Bean
+    public KieClient kieClient(KieAddressManager kieAddressManager, HttpTransport configKieHttpTransport) {
+      return new KieClient(kieAddressManager, configKieHttpTransport);
+    }
+
+    @Bean
+    public KieConfigManager kieConfigManager(KieClient kieClient, ServiceCombConfigProperties configProperties) {
+      ConfigurationsRequest configurationsRequest = createConfigurationsRequest(configProperties);
+      ConfigurationsResponse response = kieClient.queryConfigurations(configurationsRequest);
+      configurations.putAll(response.getConfigurations());
+      configurationsRequest.setRevision(response.getRevision());
+
+      KieConfigManager kieConfigManager = new KieConfigManager(kieClient, EventManager.getEventBus(), configurations);
+      kieConfigManager.setConfigurationsRequest(configurationsRequest);
+      kieConfigManager.startConfigKieManager();
+      return kieConfigManager;
+    }
+
+    private ConfigurationsRequest createConfigurationsRequest(ServiceCombConfigProperties configProperties) {
+      ConfigurationsRequest request = new ConfigurationsRequest();
+      request.setApplication(configProperties.getAppName());
+      request.setServiceName(configProperties.getServiceName());
+      request.setVersion(configProperties.getVersion());
+      request.setEnvironment(configProperties.getEnv());
+      // 需要设置为 null， 并且 query 参数为 revision=null 才会返回 revision 信息。 revision = 是不行的。
+      request.setRevision(null);
+      return request;
+    }
+  }
+
+  private static RequestAuthHeaderProvider getRequestAuthHeaderProvider(List<AuthHeaderProvider> authHeaderProviders) {
+    return signRequest -> {
+      Map<String, String> headers = new HashMap<>();
+      authHeaderProviders.forEach(provider -> headers.putAll(provider.authHeaders()));
+      return headers;
+    };
   }
 }
