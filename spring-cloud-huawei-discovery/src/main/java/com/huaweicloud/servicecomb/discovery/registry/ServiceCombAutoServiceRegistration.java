dd4d0f69510043f9287cfa550200bcfd74cd59d5
david6969xin <86217945+david6969xin@users.noreply.github.com>
2021-11-01 09:58:48 +0800

    [#541] fix an error occurs when both management.server.port and serve… (#542)

--- spring-cloud-huawei-discovery/src/main/java/com/huaweicloud/servicecomb/discovery/registry/ServiceCombAutoServiceRegistration.java
+++ spring-cloud-huawei-discovery/src/main/java/com/huaweicloud/servicecomb/discovery/registry/ServiceCombAutoServiceRegistration.java
@@ -57,6 +57,6 @@ public class ServiceCombAutoServiceRegistration extends AbstractAutoServiceRegis
 
   @Override
   protected ServiceCombRegistration getManagementRegistration() {
-    throw new IllegalStateException("not implemented");
+    return null;
   }
 }
