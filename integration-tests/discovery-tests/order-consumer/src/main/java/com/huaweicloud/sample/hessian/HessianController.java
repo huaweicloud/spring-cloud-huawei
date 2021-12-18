f186aba2c51d269e1deb09d9342741aeed116f3e
liubao68 <bismy@qq.com>
2021-12-18 11:50:55 +0800

    [#550]support hessian2 encoding

---
+++ integration-tests/discovery-tests/order-consumer/src/main/java/com/huaweicloud/sample/hessian/HessianController.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.huaweicloud.sample.hessian;
+
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hessian")
+public class HessianController {
+  @Autowired
+  private HessianService hessianService;
+
+  @RequestMapping("/testHessian")
+  public String testHessian() {
+    testBase();
+    testChildBase();
+    testGenericBase();
+    testGenericChildBase();
+    return "success";
+  }
+
+  private void testGenericChildBase() {
+    ChildBase b = new ChildBase();
+    b.setName("n");
+    b.setAge("a");
+    Generic<Base> g = new Generic<>();
+    g.setData(Arrays.asList(b));
+    Generic<Base> r = hessianService.generic(g);
+    check(r.getData().get(0) instanceof Base, "wrong type");
+    check(r.getData().get(0) instanceof ChildBase, "wrong type");
+    check("n", r.getData().get(0).getName(), "wrong value");
+    check("a", ((ChildBase) r.getData().get(0)).getAge(), "wrong value");
+  }
+
+  private void testGenericBase() {
+    Base b = new Base();
+    b.setName("n");
+    Generic<Base> g = new Generic<>();
+    g.setData(Arrays.asList(b));
+    Generic<Base> r = hessianService.generic(g);
+    check(r.getData().get(0) instanceof Base, "wrong type");
+    check(!(r.getData().get(0) instanceof ChildBase), "wrong type");
+    check("n", r.getData().get(0).getName(), "wrong value");
+  }
+
+  private void testBase() {
+    Base b = new Base();
+    b.setName("n");
+    Base r = hessianService.base(b);
+    check(r instanceof Base, "wrong type");
+    check(!(r instanceof ChildBase), "wrong type");
+    check("n", r.getName(), "wrong value");
+  }
+
+  private void testChildBase() {
+    ChildBase b = new ChildBase();
+    b.setName("n");
+    b.setAge("a");
+    Base r = hessianService.base(b);
+    check(r instanceof Base, "wrong type");
+    check(r instanceof ChildBase, "wrong type");
+    check("n", r.getName(), "wrong value");
+    check("a", ((ChildBase) r).getAge(), "wrong value");
+  }
+
+  private void check(boolean result, String error) {
+    if (!result) {
+      throw new RuntimeException(error);
+    }
+  }
+
+  private void check(String expected, String actual, String error) {
+    if (!StringUtils.equals(expected, actual)) {
+      throw new RuntimeException(error);
+    }
+  }
+}
