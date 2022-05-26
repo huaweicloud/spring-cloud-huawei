ef9900a731cbe4a38f29330c5b2a14b44d86a487
zhaozw <zhaozhongwei22@163.com>
2022-05-26 15:25:31 +0800

    trigger CI

--- integration-tests/discovery-tests/order-consumer/src/main/java/com/huaweicloud/sample/SchemaController.java
+++ integration-tests/discovery-tests/order-consumer/src/main/java/com/huaweicloud/sample/SchemaController.java
@@ -42,7 +42,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RestController
 public class SchemaController {
 
-  final ServiceCombSwaggerHandler serviceCombSwaggerHandler;
+  private final ServiceCombSwaggerHandler serviceCombSwaggerHandler;
 
   private final RestTemplate restTemplate;
 
