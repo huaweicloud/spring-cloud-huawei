b3aea02c423e9bd880fdf924263ba5ae3b977549
liubao68 <bismy@qq.com>
2022-05-30 20:36:46 +0800

    [#668] replace springfox to spring doc. Part2: servicecomb compatible generator (#698)

--- integration-tests/discovery-tests/price-provider/src/main/java/com/huaweicloud/sample/SchemaController.java
+++ integration-tests/discovery-tests/price-provider/src/main/java/com/huaweicloud/sample/SchemaController.java
@@ -1,25 +1,26 @@
 /*
 
-  * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
 
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.huaweicloud.sample;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
@@ -35,8 +36,6 @@ import com.huaweicloud.common.schema.ServiceCombSwaggerHandler;
 import io.swagger.v3.core.util.Yaml;
 import io.swagger.v3.oas.models.OpenAPI;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 /**
  * Class for testing schema generator
  */
@@ -48,9 +47,10 @@ public class SchemaController {
   @RequestMapping("/testSchemaGenerator")
   public String testSchemaGenerator() throws Exception {
     List<String> schemas = serviceCombSwaggerHandler.getSchemaIds();
-    assertThat(schemas.size()).isGreaterThan(2);
     Map<String, String> schemaContents = serviceCombSwaggerHandler.getSchemasMap();
-    assertThat(schemaContents.size()).isGreaterThan(2);
+    Map<String, String> schemaSummary = serviceCombSwaggerHandler.getSchemasSummaryMap();
+    assertThat(schemaContents.size()).isEqualTo(schemas.size());
+    assertThat(schemaContents.size()).isEqualTo(schemaSummary.size());
 
     String a1 = schemaContents.get("schemaContentController");
     String a2 = readFile("SchemaContentController.yaml");
