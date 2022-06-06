efdbfea350fb0548417206b10d1a448e1966f832
liubao <bismy@qq.com>
2022-06-06 14:20:32 +0800

    [#709]invocation context support microservice name and instance id(part1)

---
+++ spring-cloud-huawei-discovery/src/main/java/com/huaweicloud/servicecomb/discovery/context/FeignAddServiceNameContext.java
@@ -0,0 +1,38 @@
+/*
+
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.huaweicloud.servicecomb.discovery.context;
+
+import com.huaweicloud.common.adapters.feign.OrderedRequestInterceptor;
+import com.huaweicloud.common.context.InvocationContext;
+import com.huaweicloud.common.context.InvocationContextHolder;
+import com.huaweicloud.servicecomb.discovery.registry.ServiceCombRegistration;
+
+public class FeignAddServiceNameContext implements OrderedRequestInterceptor {
+  private final ServiceCombRegistration registration;
+
+  public FeignAddServiceNameContext(ServiceCombRegistration registration) {
+    this.registration = registration;
+  }
+
+  @Override
+  public void apply(feign.RequestTemplate requestTemplate) {
+    InvocationContext context = InvocationContextHolder.getOrCreateInvocationContext();
+    context.putContext(InvocationContext.CONTEXT_MICROSERVICE_NAME, registration.getServiceId());
+    context.putContext(InvocationContext.CONTEXT_INSTANCE_ID, registration.getMicroserviceInstance().getInstanceId());
+  }
+}
