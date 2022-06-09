6620d9e97246913442a77fd2113f57c76c464270
liubao <bismy@qq.com>
2022-06-09 17:52:49 +0800

    [#707]fix router client decode header may fail and using context to delivery invocation header

--- spring-cloud-huawei-router-client/src/main/java/com/huaweicloud/router/client/loabalancer/CanaryServiceInstanceFilter.java
+++ spring-cloud-huawei-router-client/src/main/java/com/huaweicloud/router/client/loabalancer/CanaryServiceInstanceFilter.java
@@ -1,30 +1,29 @@
 /*
 
-  * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
 
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.huaweicloud.router.client.loabalancer;
 
-import com.huaweicloud.router.client.track.RouterTrackContext;
-import com.huaweicloud.servicecomb.discovery.client.model.ServiceCombServiceInstance;
-import org.apache.servicecomb.foundation.common.utils.JsonUtils;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.servicecomb.router.RouterFilter;
 import org.apache.servicecomb.router.distribute.AbstractRouterDistributor;
 import org.apache.servicecomb.service.center.client.model.MicroserviceInstance;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.DefaultRequestContext;
@@ -34,33 +33,28 @@ import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.http.HttpHeaders;
 import org.springframework.util.CollectionUtils;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.huaweicloud.common.context.InvocationContext;
+import com.huaweicloud.common.context.InvocationContextHolder;
 
 public class CanaryServiceInstanceFilter implements ServiceInstanceFilter {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(CanaryServiceInstanceFilter.class);
-
   private final AbstractRouterDistributor<ServiceInstance, MicroserviceInstance> routerDistributor;
 
   private final RouterFilter routerFilter;
 
   @Autowired
-  public CanaryServiceInstanceFilter(AbstractRouterDistributor<ServiceInstance, MicroserviceInstance> routerDistributor, RouterFilter routerFilter) {
+  public CanaryServiceInstanceFilter(AbstractRouterDistributor<ServiceInstance, MicroserviceInstance> routerDistributor,
+      RouterFilter routerFilter) {
     this.routerDistributor = routerDistributor;
     this.routerFilter = routerFilter;
   }
 
   @Override
-  @SuppressWarnings({"rawtypes", "unchecked"})
   public List<ServiceInstance> filter(ServiceInstanceListSupplier supplier, List<ServiceInstance> instances,
-                                      Request<?> request) {
+      Request<?> request) {
     if (CollectionUtils.isEmpty(instances)) {
       return instances;
     }
-    String  targetServiceName = ((ServiceCombServiceInstance) instances.get(0)).getServiceId();
+    String targetServiceName = instances.get(0).getServiceId();
     DefaultRequestContext context = (DefaultRequestContext) request.getContext();
 
     Object clientRequest = context.getClientRequest();
@@ -73,16 +67,10 @@ public class CanaryServiceInstanceFilter implements ServiceInstanceFilter {
       httpHeaders = ((RequestData) clientRequest).getHeaders();
     }
 
-    Map<String, String> canaryHeaders = new HashMap<>();
-    try {
-      if (httpHeaders.getFirst(RouterTrackContext.ROUTER_TRACK_HEADER) != null) {
-        canaryHeaders.putAll(
-            JsonUtils.readValue(httpHeaders.getFirst(RouterTrackContext.ROUTER_TRACK_HEADER).getBytes(), Map.class));
-      }
-      canaryHeaders.putAll(httpHeaders.toSingleValueMap());
-    } catch (IOException e) {
-      LOGGER.warn("decode headers failed", e);
-    }
+    Map<String, String> canaryHeaders = new HashMap<>(httpHeaders.toSingleValueMap());
+
+    InvocationContext invocationContext = InvocationContextHolder.getOrCreateInvocationContext();
+    canaryHeaders.putAll(invocationContext.getContext());
 
     return routerFilter
         .getFilteredListOfServers(instances, targetServiceName, canaryHeaders,
