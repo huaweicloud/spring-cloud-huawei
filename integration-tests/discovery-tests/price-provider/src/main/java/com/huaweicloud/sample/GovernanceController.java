542ab74b59e9285c5170ad1f14dd78fbd4f2cde7
liubao68 <bismy@qq.com>
2022-06-13 14:25:45 +0800

    [#713]flow control support limit for each source server 2020.0.x 2021.0.x (#726)

--- integration-tests/discovery-tests/price-provider/src/main/java/com/huaweicloud/sample/GovernanceController.java
+++ integration-tests/discovery-tests/price-provider/src/main/java/com/huaweicloud/sample/GovernanceController.java
@@ -1,19 +1,19 @@
 /*
 
-  * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
 
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.huaweicloud.sample;
 
@@ -22,6 +22,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,7 +43,7 @@ public class GovernanceController {
 
     int retry = retryTimes.get(invocationID);
 
-    if (retry  == 3) {
+    if (retry == 3) {
       return "try times: " + retry;
     }
     response.setStatus(502);
@@ -52,5 +53,10 @@ public class GovernanceController {
   @RequestMapping("/circuitBreaker")
   public String circuitBreaker() {
     throw new RuntimeException("circuitBreaker by provider.");
+  }
+
+  @GetMapping("/rate/testRateLimitForService")
+  public String testRateLimitForService() {
+    return "success";
   }
 }
