6b01ad352d6be3483556a6a01bf46d29c61b1c76
liubao <bismy@qq.com>
2022-06-13 14:13:30 +0800

    [#713]flow control support limit for each source server 2020.0.x 2021.0.x

--- spring-cloud-huawei-governance/src/main/java/com/huaweicloud/governance/GovernanceRequestMappingHandlerAdapter.java
+++ spring-cloud-huawei-governance/src/main/java/com/huaweicloud/governance/GovernanceRequestMappingHandlerAdapter.java
@@ -1,19 +1,19 @@
 /*
 
-  * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
 
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.huaweicloud.governance;
 
 import javax.servlet.http.HttpServletRequest;
@@ -32,6 +32,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.huaweicloud.common.context.InvocationContext;
+import com.huaweicloud.common.context.InvocationContextHolder;
 import com.huaweicloud.common.util.HeaderUtil;
 
 import io.github.resilience4j.bulkhead.Bulkhead;
@@ -61,11 +63,12 @@ public class GovernanceRequestMappingHandlerAdapter {
   private ServerRecoverPolicy<Object> serverRecoverPolicy;
 
   @Autowired
-  public GovernanceRequestMappingHandlerAdapter(RateLimitingHandler rateLimitingHandler, CircuitBreakerHandler circuitBreakerHandler, BulkheadHandler bulkheadHandler) {
+  public GovernanceRequestMappingHandlerAdapter(RateLimitingHandler rateLimitingHandler,
+      CircuitBreakerHandler circuitBreakerHandler, BulkheadHandler bulkheadHandler) {
     this.rateLimitingHandler = rateLimitingHandler;
     this.circuitBreakerHandler = circuitBreakerHandler;
     this.bulkheadHandler = bulkheadHandler;
-}
+  }
 
   @Autowired(required = false)
   public void setServerRecoverPolicy(ServerRecoverPolicy<Object> serverRecoverPolicy) {
@@ -126,6 +129,10 @@ public class GovernanceRequestMappingHandlerAdapter {
     govHttpRequest.setHeaders(HeaderUtil.getHeaders(request));
     govHttpRequest.setMethod(request.getMethod());
     govHttpRequest.setUri(request.getRequestURI());
+    govHttpRequest.setServiceName(InvocationContextHolder
+        .getOrCreateInvocationContext().getContext(InvocationContext.CONTEXT_MICROSERVICE_NAME));
+    govHttpRequest.setInstanceId(InvocationContextHolder
+        .getOrCreateInvocationContext().getContext(InvocationContext.CONTEXT_INSTANCE_ID));
     return govHttpRequest;
   }
 
