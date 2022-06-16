2577bd16acf014e5c26e16c54bc8a75f279e8722
chengyouling <97039406+chengyouling@users.noreply.github.com>
2022-06-16 14:57:23 +0800

    [#732]heartBeatInterval parse millis (#736)

--- spring-cloud-huawei-common/src/main/java/com/huaweicloud/common/transport/DiscoveryBootstrapProperties.java
+++ spring-cloud-huawei-common/src/main/java/com/huaweicloud/common/transport/DiscoveryBootstrapProperties.java
@@ -49,7 +49,7 @@ public class DiscoveryBootstrapProperties {
   private boolean healthCheck = true;
 
 
-  private int healthCheckInterval = 15000;
+  private int healthCheckInterval = 15;
 
   private int healthCheckRequestTimeout = 5000;
 
@@ -69,6 +69,10 @@ public class DiscoveryBootstrapProperties {
   private boolean ignoreSwaggerDifferent;
 
   private DataCenterInfo datacenter;
+
+  private static final int HEALTH_CHECK_MAX_INTERVAL = 600;
+
+  private static final int HEALTH_CHECK_MIN_INTERVAL = 1;
 
   public String getServerAddress() {
     return serverAddress;
@@ -171,7 +175,9 @@ public class DiscoveryBootstrapProperties {
   }
 
   public void setHealthCheckInterval(int healthCheckInterval) {
-    this.healthCheckInterval = healthCheckInterval;
+    if (healthCheckInterval <= HEALTH_CHECK_MAX_INTERVAL && healthCheckInterval >= HEALTH_CHECK_MIN_INTERVAL) {
+      this.healthCheckInterval = healthCheckInterval;
+    }
   }
 
   public int getHealthCheckRequestTimeout() {
