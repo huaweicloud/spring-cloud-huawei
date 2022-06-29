53349fdc380ec154e6477a3859b077a9d6fa0ff4
liubao68 <bismy@qq.com>
2022-06-29 18:23:21 +0800

    [#772]add Graceful Shutdown to hoxton (#773)

--- spring-cloud-huawei-discovery/src/main/java/com/huaweicloud/servicecomb/discovery/registry/ServiceCombRegistryAutoConfiguration.java
+++ spring-cloud-huawei-discovery/src/main/java/com/huaweicloud/servicecomb/discovery/registry/ServiceCombRegistryAutoConfiguration.java
@@ -1,19 +1,19 @@
 /*
 
-  * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
 
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.huaweicloud.servicecomb.discovery.registry;
 
@@ -32,6 +32,7 @@ import org.springframework.cloud.client.serviceregistry.ServiceRegistryAutoConfi
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.huaweicloud.common.event.ClosedEventListener;
 import com.huaweicloud.common.transport.DiscoveryBootstrapProperties;
 import com.huaweicloud.servicecomb.discovery.ConditionalOnServiceCombEnabled;
 import com.huaweicloud.servicecomb.discovery.DiscoveryAutoConfiguration;
@@ -53,10 +54,11 @@ public class ServiceCombRegistryAutoConfiguration {
   @Bean
   public ServiceCombServiceRegistry serviceCombServiceRegistry(
       DiscoveryBootstrapProperties discoveryBootstrapProperties,
+      ClosedEventListener closedEventListener,
       ServiceCenterClient serviceCenterClient,
       ServiceCenterWatch serviceCenterWatch) {
     return new ServiceCombServiceRegistry(
-        discoveryBootstrapProperties, serviceCenterClient, serviceCenterWatch);
+        discoveryBootstrapProperties, closedEventListener, serviceCenterClient, serviceCenterWatch);
   }
 
   @Bean
