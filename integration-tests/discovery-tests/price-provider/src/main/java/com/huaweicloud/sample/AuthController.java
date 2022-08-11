ce235c5fe8b10667791df3612dd0c112afcc1a1c
zhaozw <zhaozhongwei22@163.com>
2022-08-11 11:12:15 +0800

    add test

---
+++ integration-tests/discovery-tests/price-provider/src/main/java/com/huaweicloud/sample/AuthController.java
@@ -0,0 +1,59 @@
+/*
+
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.huaweicloud.sample;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.huaweicloud.common.context.InvocationContext;
+import com.huaweicloud.common.context.InvocationContextHolder;
+import com.huaweicloud.governance.authentication.AuthHandlerBoot;
+import com.huaweicloud.governance.authentication.Const;
+import com.huaweicloud.governance.authentication.provider.BlackWhiteListProperties;
+import com.huaweicloud.governance.authentication.provider.ProviderAuthHandler;
+
+@RestController
+public class AuthController {
+
+  private final ApplicationContext applicationContext;
+
+  @Autowired
+  public AuthController(ApplicationContext applicationContext) {
+    this.applicationContext = applicationContext;
+  }
+
+  @RequestMapping("/checkToken")
+  public String checkToken() {
+    ProviderAuthHandler providerAuthHandler = applicationContext.getBean(ProviderAuthHandler.class);
+    AuthHandlerBoot authHandlerBoot = applicationContext.getBean(AuthHandlerBoot.class);
+    BlackWhiteListProperties blackWhiteListProperties = applicationContext.getBean(BlackWhiteListProperties.class);
+    if (providerAuthHandler == null || authHandlerBoot == null || blackWhiteListProperties == null
+        || blackWhiteListProperties.getBlack().size() != 2 || blackWhiteListProperties.getWhite().size() != 1) {
+      return null;
+    }
+
+    InvocationContext invocationContext = InvocationContextHolder.getOrCreateInvocationContext();
+    if (StringUtils.isEmpty(invocationContext.getContext(Const.AUTH_TOKEN))) {
+      return null;
+    }
+    return "success";
+  }
+
+}
