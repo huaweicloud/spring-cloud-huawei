0a9fc7821feca2e2533bca745f9a96886d62124b
liubao68 <bismy@qq.com>
2022-08-15 08:40:19 +0800

    [#835]add identifier rate limiting for gateway (#837)

--- integration-tests/discovery-tests/discovery-tests-client/src/test/java/com/huaweicloud/sample/GatewayIT.java
+++ integration-tests/discovery-tests/discovery-tests-client/src/test/java/com/huaweicloud/sample/GatewayIT.java
@@ -161,4 +161,53 @@ public class GatewayIT {
     Assertions.assertFalse(notExpectedFailed.get());
     Assertions.assertTrue(successCount.get() >= 10);
   }
+
+  @Test
+  public void testIdentifierRateLimiting() throws Exception {
+    for (int i = 0; i < 10; i++) {
+      testIdentifierRateLimiting("user" + i);
+    }
+  }
+
+  private void testIdentifierRateLimiting(String userId) throws Exception {
+    CountDownLatch latch = new CountDownLatch(10);
+    AtomicBoolean expectedFailed = new AtomicBoolean(false);
+    AtomicBoolean notExpectedFailed = new AtomicBoolean(false);
+    AtomicLong successCount = new AtomicLong(0);
+
+    for (int i = 0; i < 1; i++) {
+      for (int j = 0; j < 10; j++) {
+        String name = "t-" + i + "-" + j;
+        new Thread(name) {
+          public void run() {
+            try {
+              HttpHeaders headers = new HttpHeaders();
+              headers.add("user-id", userId);
+              HttpEntity<Void> entity = new HttpEntity<>(headers);
+              String result = template.exchange(url + "/identifierRateLimiting", HttpMethod.GET, entity,
+                  String.class).getBody();
+              if (!"OK".equals(result)) {
+                notExpectedFailed.set(true);
+              } else {
+                successCount.getAndIncrement();
+              }
+            } catch (Exception e) {
+              if (e instanceof HttpClientErrorException && ((HttpClientErrorException) e).getRawStatusCode() == 429) {
+                expectedFailed.set(true);
+              } else {
+                notExpectedFailed.set(true);
+              }
+            }
+            latch.countDown();
+          }
+        }.start();
+      }
+      Thread.sleep(100);
+    }
+
+    latch.await(20, TimeUnit.SECONDS);
+    Assertions.assertTrue(expectedFailed.get());
+    Assertions.assertFalse(notExpectedFailed.get());
+    Assertions.assertTrue(successCount.get() >= 2);
+  }
 }
