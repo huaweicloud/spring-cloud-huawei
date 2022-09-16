beaf0fdad7ea16e41d88e7387a7e5b7dc0c4fc0e
lbc97 <82015813+lbc97@users.noreply.github.com>
2022-09-16 11:49:15 +0800

    [#914] upgrade jaspyt to fix refresh event problem (#916)

--- integration-tests/cse-v2/provider/src/main/java/com/huaweicloud/samples/ProviderController.java
+++ integration-tests/cse-v2/provider/src/main/java/com/huaweicloud/samples/ProviderController.java
@@ -1,44 +1,50 @@
 /*
 
-  * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
 
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.huaweicloud.samples;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ProviderController {
   @Value("${spring.cloud.huawei.test.username}")
-  private  String username;
+  private String username;
+
   @Value("${spring.cloud.huawei.test.password}")
-  private  String password;
+  private String password;
+
   // a very simple service to echo the request parameter
   @GetMapping("/sayHello")
   public String sayHello(@RequestParam("name") String name) {
     return "Hello " + name;
   }
+
   @GetMapping("/jasypt1")
-  public String jasypt1(){
+  public String jasypt1() {
     return username;
   }
+
   @GetMapping("/jasypt2")
-  public String jasypt2(){
+  public String jasypt2() {
     return password;
   }
 }
