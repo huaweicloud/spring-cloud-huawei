eb6e6e6e0664dcadba2b4162a75566030c5bb229
liubao <bismy@qq.com>
2022-11-08 19:11:35 +0800

    [#931]make servicecomb discovery order configurable

--- spring-cloud-huawei-discovery/src/main/java/com/huaweicloud/servicecomb/discovery/discovery/ServiceCombReactiveDiscoveryClient.java
+++ spring-cloud-huawei-discovery/src/main/java/com/huaweicloud/servicecomb/discovery/discovery/ServiceCombReactiveDiscoveryClient.java
@@ -1,19 +1,19 @@
 /*
 
-  * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
 
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.huaweicloud.servicecomb.discovery.discovery;
 
 import org.springframework.cloud.client.ServiceInstance;
@@ -25,9 +25,9 @@ import reactor.core.scheduler.Schedulers;
 
 public class ServiceCombReactiveDiscoveryClient implements ReactiveDiscoveryClient {
 
-  private final DiscoveryClient discoveryClient;
+  private final ServiceCombDiscoveryClient discoveryClient;
 
-  public ServiceCombReactiveDiscoveryClient(DiscoveryClient discoveryClient) {
+  public ServiceCombReactiveDiscoveryClient(ServiceCombDiscoveryClient discoveryClient) {
     this.discoveryClient = discoveryClient;
   }
 
@@ -46,5 +46,10 @@ public class ServiceCombReactiveDiscoveryClient implements ReactiveDiscoveryClie
   public Flux<String> getServices() {
     return Flux.defer(() -> Flux.fromIterable(discoveryClient.getServices()))
         .subscribeOn(Schedulers.boundedElastic());
+  }
+
+  @Override
+  public int getOrder() {
+    return discoveryClient.getOrder();
   }
 }
