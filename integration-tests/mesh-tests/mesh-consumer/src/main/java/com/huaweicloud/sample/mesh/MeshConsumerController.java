296db60d3fd72e33891a58590efe8feb322bb8ae
liubao68 <bismy@qq.com>
2022-11-08 18:53:04 +0800

    [#929]support mesh discovery client (#930)

---
+++ integration-tests/mesh-tests/mesh-consumer/src/main/java/com/huaweicloud/sample/mesh/MeshConsumerController.java
@@ -0,0 +1,49 @@
+/*
+
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.huaweicloud.sample.mesh;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/mesh/consumer")
+public class MeshConsumerController {
+  private final MeshProviderService meshProviderService;
+
+  private final RestTemplate restTemplate;
+
+  @Autowired
+  public MeshConsumerController(MeshProviderService meshProviderService,
+      RestTemplate restTemplate) {
+    this.meshProviderService = meshProviderService;
+    this.restTemplate = restTemplate;
+  }
+
+  @GetMapping("/sayHelloFeign")
+  public String sayHelloFeign(@RequestParam("name") String name) {
+    return meshProviderService.sayHello(name);
+  }
+
+  @GetMapping("/sayHelloRestTemplate")
+  public String sayHelloRestTemplate(@RequestParam("name") String name) {
+    return restTemplate.getForObject("http://mesh-provider/mesh/provider/sayHello?name={1}", String.class, name);
+  }
+}
---
+++ spring-cloud-huawei-discovery/src/main/java/com/huaweicloud/mesh/discovery/MeshReactiveDiscoveryClient.java
@@ -0,0 +1,60 @@
+/*
+
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.huaweicloud.mesh.discovery;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+
+import reactor.core.publisher.Flux;
+
+public class MeshReactiveDiscoveryClient implements ReactiveDiscoveryClient {
+  private final MeshDiscoveryProperties meshDiscoveryProperties;
+
+  public MeshReactiveDiscoveryClient(MeshDiscoveryProperties meshDiscoveryProperties) {
+    this.meshDiscoveryProperties = meshDiscoveryProperties;
+  }
+
+  @Override
+  public String description() {
+    return "Client for docker or k8s";
+  }
+
+  @Override
+  public Flux<ServiceInstance> getInstances(String serviceId) {
+    return Flux.just(meshDiscoveryProperties.toServiceInstance(serviceId));
+  }
+
+  @Override
+  public Flux<String> getServices() {
+    // if
+    // spring:
+    //  cloud:
+    //    gateway:
+    //      discovery:
+    //        locator:
+    //          enabled: true
+    // mush return all actual services. When this enabled in gateway,
+    // can not use mesh discovery
+    return Flux.empty();
+  }
+
+  @Override
+  public int getOrder() {
+    return meshDiscoveryProperties.getOrder();
+  }
+}
