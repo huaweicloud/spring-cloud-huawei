243dad17c9d7c7ae42bfa4043134c1c7affc356b
liubao68 <bismy@qq.com>
2022-11-08 18:53:04 +0800

    [#929]support mesh discovery client (#930)

---
+++ spring-cloud-huawei-discovery/src/main/java/com/huaweicloud/mesh/discovery/MeshDiscoveryProperties.java
@@ -0,0 +1,84 @@
+/*
+
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.huaweicloud.mesh.discovery;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.DefaultServiceInstance;
+import org.springframework.cloud.client.ServiceInstance;
+
+@ConfigurationProperties(prefix = "spring.cloud.discovery.client.mesh")
+public class MeshDiscoveryProperties {
+  private int order = 200;
+
+  private String host;
+
+  private int port;
+
+  private boolean secure;
+
+  private Map<String, String> metadata = new LinkedHashMap<>();
+
+  public ServiceInstance toServiceInstance(String serviceId) {
+    return new DefaultServiceInstance(
+        serviceId, serviceId, host,
+        port, secure, metadata);
+  }
+
+  public String getHost() {
+    return host;
+  }
+
+  public void setHost(String host) {
+    this.host = host;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
+
+  public boolean isSecure() {
+    return secure;
+  }
+
+  public void setSecure(boolean secure) {
+    this.secure = secure;
+  }
+
+  public Map<String, String> getMetadata() {
+    return metadata;
+  }
+
+  public void setMetadata(Map<String, String> metadata) {
+    this.metadata = metadata;
+  }
+
+  public int getOrder() {
+    return order;
+  }
+
+  public void setOrder(int order) {
+    this.order = order;
+  }
+}
