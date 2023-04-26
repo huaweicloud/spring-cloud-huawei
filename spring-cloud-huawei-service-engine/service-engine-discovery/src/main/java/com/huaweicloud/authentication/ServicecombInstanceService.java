250bc68da669ebb61d56af4088deb8143308775e
chengyouling <spadgerlin888@163.com>
2023-04-26 11:46:21 +0800

    fixed CI problem

--- spring-cloud-huawei-service-engine/service-engine-discovery/src/main/java/com/huaweicloud/authentication/ServicecombInstanceService.java
+++ spring-cloud-huawei-service-engine/service-engine-discovery/src/main/java/com/huaweicloud/authentication/ServicecombInstanceService.java
@@ -28,7 +28,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.beans.PropertyDescriptor;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
