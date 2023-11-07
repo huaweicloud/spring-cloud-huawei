8a82292416c4d13be56735d0c66e291613817984
chengyouling <spadgerlin888@163.com>
2023-11-07 20:12:38 +0800

    update notes

--- spring-cloud-huawei-governance/src/main/java/com/huaweicloud/governance/authentication/RSAProviderTokenManager.java
+++ spring-cloud-huawei-governance/src/main/java/com/huaweicloud/governance/authentication/RSAProviderTokenManager.java
@@ -43,11 +43,11 @@ public class RSAProviderTokenManager {
   }
 
   /**
-   * 1.tokenCheckEnabled is true or request headers has no serviceName, use serviceId and instanceId for authenration
+   * 1.tokenCheckEnabled is true or request headers has no serviceName, use serviceId and instanceId for authenticaation
    * in token.
-   * 2.tokenCheckEnabled is false and request headers has serviceName, use serviceName for authenration in request
+   * 2.tokenCheckEnabled is false and request headers has serviceName, use serviceName for authentication in request
    * header.
-   * 
+   *
    * @param request
    * @throws Exception
    */
