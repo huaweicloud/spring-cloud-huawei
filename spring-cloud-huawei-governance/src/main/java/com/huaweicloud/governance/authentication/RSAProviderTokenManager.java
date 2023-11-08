522f0c8f3dede59ce616425df8cdec9b3c0d5c0f
Cheng YouLing <97039406+chengyouling@users.noreply.github.com>
2023-11-08 16:28:29 +0800

    [#1092] optimize security authentication verification logic (#1093)

--- spring-cloud-huawei-governance/src/main/java/com/huaweicloud/governance/authentication/RSAProviderTokenManager.java
+++ spring-cloud-huawei-governance/src/main/java/com/huaweicloud/governance/authentication/RSAProviderTokenManager.java
@@ -18,6 +18,8 @@
 package com.huaweicloud.governance.authentication;
 
 import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
@@ -40,14 +42,21 @@ public class RSAProviderTokenManager {
     this.authenticationAdapter = authenticationAdapter;
   }
 
+  /**
+   * 1.tokenCheckEnabled is true or request headers has no serviceName, use serviceId and instanceId for authentication
+   * in token.
+   * 2.tokenCheckEnabled is false and request headers has serviceName, use serviceName for authentication in request
+   * header.
+   *
+   * @param request
+   * @throws Exception
+   */
   public void valid(HttpServletRequest request) throws Exception {
     try {
-      RsaAuthenticationToken rsaToken = null;
-      if (environment.getProperty(Const.AUTH_TOKEN_CHECK_ENABLED, boolean.class, true)) {
-        rsaToken = RSATokenCheckUtils.checkTokenInfo(request, authenticationAdapter);
-      }
       AuthRequestExtractor extractor;
-      if (rsaToken != null) {
+      if (environment.getProperty(Const.AUTH_TOKEN_CHECK_ENABLED, boolean.class, true)
+          || StringUtils.isEmpty(request.getHeader(Const.AUTH_SERVICE_NAME))) {
+        RsaAuthenticationToken rsaToken = RSATokenCheckUtils.checkTokenInfo(request, authenticationAdapter);
         extractor = AuthRequestExtractorUtils.createAuthRequestExtractor(request, rsaToken.getServiceId(),
             rsaToken.getInstanceId());
       } else {
