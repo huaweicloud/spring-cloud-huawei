e8f8f2a74c20765794e136b08f6420a4eeadeaa5
chengyouling <spadgerlin888@163.com>
2023-11-08 14:23:16 +0800

    [#1092] optimize security authentication verification logic

--- spring-cloud-huawei-governance/src/main/java/com/huaweicloud/governance/authentication/RSAProviderTokenManager.java
+++ spring-cloud-huawei-governance/src/main/java/com/huaweicloud/governance/authentication/RSAProviderTokenManager.java
@@ -21,6 +21,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
@@ -43,12 +44,10 @@ public class RSAProviderTokenManager {
 
   public void valid(HttpServletRequest request) throws Exception {
     try {
-      RsaAuthenticationToken rsaToken = null;
-      if (environment.getProperty(Const.AUTH_TOKEN_CHECK_ENABLED, boolean.class, true)) {
-        rsaToken = RSATokenCheckUtils.checkTokenInfo(request, authenticationAdapter);
-      }
       AuthRequestExtractor extractor;
-      if (rsaToken != null) {
+      if (environment.getProperty(Const.AUTH_TOKEN_CHECK_ENABLED, boolean.class, true)
+          || StringUtils.isEmpty(request.getHeader(Const.AUTH_SERVICE_NAME))) {
+        RsaAuthenticationToken rsaToken = RSATokenCheckUtils.checkTokenInfo(request, authenticationAdapter);
         extractor = AuthRequestExtractorUtils.createAuthRequestExtractor(request, rsaToken.getServiceId(),
             rsaToken.getInstanceId());
       } else {
