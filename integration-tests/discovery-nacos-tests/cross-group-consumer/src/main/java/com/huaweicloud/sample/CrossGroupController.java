b23f9261a761064b8fb0404a2d02144e25ecaf73
Cheng YouLing <97039406+chengyouling@users.noreply.github.com>
2023-11-17 11:33:53 +0800

    [#1095] nacos support cross group invoking (#1096)

---
+++ integration-tests/discovery-nacos-tests/cross-group-consumer/src/main/java/com/huaweicloud/sample/CrossGroupController.java
@@ -0,0 +1,40 @@
+/*
+
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.huaweicloud.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CrossGroupController {
+
+  private final RestTemplate restTemplate;
+
+  @Autowired
+  public CrossGroupController(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
+
+  @RequestMapping("/cross-group")
+  public String crossGroup(@RequestParam("id") String id) {
+    String callServiceResult = restTemplate.getForObject("http://price/price?id=" + id, String.class);
+    return callServiceResult;
+  }
+}
