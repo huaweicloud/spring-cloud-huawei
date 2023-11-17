fed603a72c2cedc77c52496185406c492237f57d
Cheng YouLing <97039406+chengyouling@users.noreply.github.com>
2023-11-17 17:29:53 +0800

    [#1101] hessian feign response null body problem (#1103)

--- spring-cloud-starter-huawei/spring-cloud-starter-huawei-hessian/src/main/java/org/springframework/cloud/openfeign/support/ExtendedSpringDecoder.java
+++ spring-cloud-starter-huawei/spring-cloud-starter-huawei-hessian/src/main/java/org/springframework/cloud/openfeign/support/ExtendedSpringDecoder.java
@@ -99,6 +99,9 @@ public class ExtendedSpringDecoder implements Decoder {
     @Override
     public void close() {
       try {
+        if (response.body() == null) {
+          return;
+        }
         response.body().close();
       } catch (IOException ex) {
         // Ignore exception on close...
@@ -107,6 +110,9 @@ public class ExtendedSpringDecoder implements Decoder {
 
     @Override
     public InputStream getBody() throws IOException {
+      if (response.body() == null) {
+        return null;
+      }
       return response.body().asInputStream();
     }
 
