1302c876fb8c9cf0bc0b23a5e03f59be0300f3d1
Cheng YouLing <97039406+chengyouling@users.noreply.github.com>
2023-11-20 10:07:06 +0800

    [#1095] nacos support cross group invoking (#1102)

---
+++ integration-tests/discovery-nacos-tests/cross-group-consumer/src/main/java/com/huaweicloud/sample/CrossGroupApplication.java
@@ -0,0 +1,45 @@
+/*
+
+  * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package com.huaweicloud.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class CrossGroupApplication {
+
+  public static void main(String[] args) {
+    try {
+      SpringApplication.run(CrossGroupApplication.class, args);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @LoadBalanced
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+}
