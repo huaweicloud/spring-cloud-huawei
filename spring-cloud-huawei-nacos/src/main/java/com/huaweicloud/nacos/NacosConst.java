5ce005095227c024b3118e735b9077adfebf2b3a
chengyouling <spadgerlin888@163.com>
2023-11-28 16:31:23 +0800

    [#1113] support load systemManage property sources for extension

---
+++ spring-cloud-huawei-nacos/src/main/java/com/huaweicloud/nacos/NacosConst.java
@@ -0,0 +1,33 @@
+/*
+
+ * Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.huaweicloud.nacos;
+
+public class NacosConst {
+  public static final String SERVICECOMB_NACOS_CONFIG_DATA_ID = "spring.cloud.servicecomb.nacos.config.dataId";
+
+  public static final String SERVICECOMB_NACOS_CONFIG_GROUP = "spring.cloud.servicecomb.nacos.config.group";
+
+  public static final String SERVICECOMB_NACOS_CONFIG_REFRESH = "spring.cloud.servicecomb.nacos.config.refresh";
+
+  public static final String SERVICECOMB_NACOS_CONFIG_TIMEOUT = "spring.cloud.servicecomb.nacos.config.timeout";
+
+  public static final String SERVICECOMB_NACOS_PROPERTY_SOURCE_NAME = "SERVICECOMB-NACOS";
+
+  public static final String SECURITY_CONFIG_DATA_ID_PREFIX = "cse-app-security-";
+
+  public static final String SECURITY_CONFIG_DATA_ID_SUFFIX = ".yaml";
+}
