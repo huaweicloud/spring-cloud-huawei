4cbb266bbe451e8cd814bfb3b41c57f132ac72fc
chengyouling <spadgerlin888@163.com>
2024-01-22 15:27:55 +0800

    [#1196] update license 2020-2022 to 2020-2024

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 #
 ## ---------------------------------------------------------------------------
 ##
-## Copyright (C) 2020-2022 Huawei Technologies Co., Ltd. All rights reserved.
+## Copyright (C) 2020-2024 Huawei Technologies Co., Ltd. All rights reserved.
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License");
 ## you may not use this file except in compliance with the License.
