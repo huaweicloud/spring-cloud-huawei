5ca4a8103fcc8030b4a02ec473fc3b33ff0dc5eb
chengyouling <spadgerlin888@163.com>
2024-03-22 16:34:30 +0800

    fixed ci

--- spring-cloud-huawei-governance/src/main/java/com/huaweicloud/governance/adapters/loadbalancer/InstanceIsolationServiceInstanceFilter.java
+++ spring-cloud-huawei-governance/src/main/java/com/huaweicloud/governance/adapters/loadbalancer/InstanceIsolationServiceInstanceFilter.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
