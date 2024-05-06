ec62b4ed375fe0fc5ac2935bd8f093ca97698e39
Cheng YouLing <97039406+chengyouling@users.noreply.github.com>
2024-05-06 15:34:00 +0800

    [#1263] api authentication add includePath and excludePath for auth (#1266)

--- spring-cloud-huawei-governance/src/test/java/com/huaweicloud/governance/authentication/securityPolicy/WebMvcRSAProviderAuthManagerTest.java
+++ spring-cloud-huawei-governance/src/test/java/com/huaweicloud/governance/authentication/securityPolicy/WebMvcRSAProviderAuthManagerTest.java
@@ -35,47 +35,193 @@ public class WebMvcRSAProviderAuthManagerTest {
   public void testCheckUriWhitelistPrefixMatch() {
     Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_WHITELIST, String.class, ""))
         .thenReturn("*/whitelist");
-    Assertions.assertTrue(manager.checkUriWhitelist("/api/check/whitelist"));
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/whitelist"));
+  }
+
+  @Test
+  public void testCheckUriExcludePrefixMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_EXCLUDE, String.class, ""))
+        .thenReturn("*/whitelist");
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriIncludePrefixMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_INCLUDE, String.class, ""))
+        .thenReturn("*/whitelist");
+    Assertions.assertTrue(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
   }
 
   @Test
   public void testCheckUriWhitelistPrefixSlashMatch() {
     Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_WHITELIST, String.class, ""))
         .thenReturn("/*/whitelist");
-    Assertions.assertTrue(manager.checkUriWhitelist("/api/check/whitelist"));
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
   }
+
+  @Test
+  public void testCheckUriExcludePrefixSlashMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_EXCLUDE, String.class, ""))
+        .thenReturn("/*/whitelist");
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriIncludePrefixSlashMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_INCLUDE, String.class, ""))
+        .thenReturn("/*/whitelist");
+    Assertions.assertTrue(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
   @Test
   public void testCheckUriWhitelistPrefixNotMatch() {
     Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_WHITELIST, String.class, ""))
         .thenReturn("*/whitelist");
-    Assertions.assertFalse(manager.checkUriWhitelist("/api/check/query"));
+    Assertions.assertTrue(manager.isRequiredAuth("/api/check/query"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriExcludePrefixNotMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_EXCLUDE, String.class, ""))
+        .thenReturn("*/whitelist");
+    Assertions.assertTrue(manager.isRequiredAuth("/api/check/query"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriIncludePrefixNotMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_INCLUDE, String.class, ""))
+        .thenReturn("*/whitelist");
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/query"));
+    clearEnv();
   }
 
   @Test
   public void testCheckUriWhitelistSuffixMatch() {
     Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_WHITELIST, String.class, ""))
         .thenReturn("/api/*");
-    Assertions.assertTrue(manager.checkUriWhitelist("/api/check/whitelist"));
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriExcludeSuffixMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_EXCLUDE, String.class, ""))
+        .thenReturn("/api/*");
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriIncludeSuffixMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_INCLUDE, String.class, ""))
+        .thenReturn("/api/*");
+    Assertions.assertTrue(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
   }
 
   @Test
   public void testCheckUriWhitelistSuffixNotMatch() {
     Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_WHITELIST, String.class, ""))
         .thenReturn("/apg/*");
-    Assertions.assertFalse(manager.checkUriWhitelist("/api/check/whitelist"));
+    Assertions.assertTrue(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriExcludeSuffixNotMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_EXCLUDE, String.class, ""))
+        .thenReturn("/apg/*");
+    Assertions.assertTrue(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriIncludeSuffixNotMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_INCLUDE, String.class, ""))
+        .thenReturn("/apg/*");
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
   }
 
   @Test
   public void testCheckUriWhitelistEqualMatch() {
     Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_WHITELIST, String.class, ""))
         .thenReturn("/api/check/whitelist");
-    Assertions.assertTrue(manager.checkUriWhitelist("/api/check/whitelist"));
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriExcludeEqualMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_EXCLUDE, String.class, ""))
+        .thenReturn("/api/check/whitelist");
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriIncludeEqualMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_INCLUDE, String.class, ""))
+        .thenReturn("/api/check/whitelist");
+    Assertions.assertTrue(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
   }
 
   @Test
   public void testCheckUriWhitelistEqualNotMatch() {
     Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_WHITELIST, String.class, ""))
         .thenReturn("/api/check/white");
-    Assertions.assertFalse(manager.checkUriWhitelist("/api/check/whitelist"));
+    Assertions.assertTrue(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriExcludeEqualNotMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_EXCLUDE, String.class, ""))
+        .thenReturn("/api/check/white");
+    Assertions.assertTrue(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriIncludeEqualNotMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_INCLUDE, String.class, ""))
+        .thenReturn("/api/check/white");
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/whitelist"));
+    clearEnv();
+  }
+
+  @Test
+  public void testCheckUriAllNotSet() {
+    Assertions.assertTrue(manager.isRequiredAuth("/api/check/whitelist"));
+  }
+
+  /**
+   * include and exclude both math url, no auth
+   */
+  @Test
+  public void testCheckUriBothMatch() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_INCLUDE, String.class, ""))
+        .thenReturn("/api/check/white");
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_EXCLUDE, String.class, ""))
+        .thenReturn("/api/check/white");
+    Assertions.assertFalse(manager.isRequiredAuth("/api/check/white"));
+    clearEnv();
+  }
+
+  private void clearEnv() {
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_INCLUDE, String.class, ""))
+        .thenReturn("");
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_EXCLUDE, String.class, ""))
+        .thenReturn("");
+    Mockito.when(environment.getProperty(GovernanceConst.AUTH_API_PATH_WHITELIST, String.class, ""))
+        .thenReturn("");
   }
 }
